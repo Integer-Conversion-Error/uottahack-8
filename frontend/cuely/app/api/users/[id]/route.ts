f51a4f4a67b42867282d8e968e9e2e2faf8b896a
jo gurvantamir <67881725+jo-walker@users.noreply.github.com>
2026-01-17 00:56:03 -0500

    Revert "wip: backend stuff setup"
    This reverts commit 2114fc700b1285a065350a07c5ee825dbba93788.

--- frontend/cuely/app/api/users/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    const { id } = params;
-    // TODO: Fetch user by ID from DB
-    return NextResponse.json({ id, name: 'Placeholder User' });
-}
